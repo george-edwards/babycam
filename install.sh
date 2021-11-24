1001018f405ac3b7a66d04f3dba0e1793624b250
George Edwards <georgeedwardsmail@gmail.com>
2021-11-24 16:26:32 +1100

    install.sd

--- install.sh
+++ install.sh
@@ -11,12 +11,11 @@ sudo mkdir -p /var/lib/motioneye
 sudo cp /home/pi/babycam/motioneye-files/motioneye.conf /etc/motioneye/motioneye.conf
 sudo cp /home/pi/babycam/motioneye-files/motion.conf /etc/motioneye/motion.conf
 sudo cp /home/pi/babycam/motioneye-files/camera-1.conf /etc/motioneye/camera-1.conf
-sudo cp /usr/local/share/motioneye/extra/motioneye.systemd-unit-local /etc/systemd/system/motioneye.service
+sudo cp /home/pi/.local/share/motioneye/extra/motioneye.systemd-unit-local /etc/systemd/system/motioneye.service
 
 echo "Configuring systemd daemons"
 sudo cp /home/pi/babycam/systemd-files/babycam* /lib/systemd/system/
 sudo systemctl daemon-reload
-sleep 2
 sudo systemctl enable babycam-add-text-overlay.service
 sudo systemctl enable babycam-infrared-cleanup.service
 sudo systemctl enable babycam-led-disable.service
